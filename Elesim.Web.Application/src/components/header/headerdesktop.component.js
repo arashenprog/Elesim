78dd19ae751c01ded34c07f292cb81a798c24d9c
asafari72 <asafari72@gmail.com>
2019-02-18 12:43:00 -0800

    add popup creditmod in header

--- Elesim.Web.Application/src/components/header/headerdesktop.component.js
+++ Elesim.Web.Application/src/components/header/headerdesktop.component.js
@@ -1,29 +1,38 @@
 import React, { Component } from "react";
-import { Menu, Icon, Button } from "antd";
+import { Radio, Menu, Icon, Button, Input, Form, Modal } from "antd";
+
 import { Link } from "react-router-dom";
 import Api from "../../services/api";
-import { withRouter } from 'react-router'
+import { withRouter } from "react-router";
 
+const FormItem = Form.Item;
+const RadioGroup = Radio.Group;
 const ButtonGroup = Button.Group;
 class HeaderDesktopComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drawerVisible: false, userToken: "", credit: "", name: "", user: null
+      drawerVisible: false,
+      userToken: "",
+      credit: "",
+      name: "",
+      user: null,
+      showCreditModal: false,
+      customCreditAmount: null,
     };
-    this.handleMenuClick = this.handleMenuClick.bind(this)
+    this.handleMenuClick = this.handleMenuClick.bind(this);
   }
   handleMenuClick(e) {
-    console.log(e)
+    console.log(e);
     switch (e.key) {
       case "1":
-        this.props.history.push("/")
+        this.props.history.push("/");
         break;
       case "2":
-        this.props.history.push("/application")
+        this.props.history.push("/application");
         break;
       case "3":
-        this.props.history.push("/contact")
+        this.props.history.push("/contact");
         break;
       default:
         break;
@@ -35,9 +44,20 @@ class HeaderDesktopComponent extends Component {
     //   this.setState({user:user})
 
     // }
-    Api.getAcyncLocalStorage("User").then(res => {
-      this.setState({ user: res })
-    }).catch(err=>console.log(err))
+    Api.getAcyncLocalStorage("User")
+      .then(res => {
+        this.setState({ user: res });
+      })
+      .catch(err => console.log(err));
+  }
+  toggleCreditModal(e) {
+    this.setState({ showCreditModal: e });
+  }
+  onCreditAmountChange = (e) => {
+    console.log('radio checked', e.target.value);
+    this.setState({
+      creditAmount: e.target.value,
+    });
   }
   render() {
     return (
@@ -62,69 +82,108 @@ class HeaderDesktopComponent extends Component {
                   defaultSelectedKeys={["0"]}
                   style={{ lineHeight: "64px" }}
                   onClick={this.handleMenuClick}
-
                 >
                   <Menu.Item key="1">
                     <Icon type="home" style={styles.icons} />
-                    &nbsp;
-                    صفحه نخست
+                    &nbsp; صفحه نخست
                   </Menu.Item>
                   <Menu.Item key="2">
                     <Icon type="mobile" style={styles.icons} />
-                    &nbsp;
-                     اپلیکیشن اِلِ سیم
+                    &nbsp; اپلیکیشن اِلِ سیم
                   </Menu.Item>
                   <Menu.Item key="3">
-                    <Icon type="phone" style={styles.icons} /> 
-                    &nbsp;
-                    تماس با ما
+                    <Icon type="phone" style={styles.icons} />
+                    &nbsp; تماس با ما
                   </Menu.Item>
                 </Menu>
               </div>
               <div className="col">
-                {this.state.user !== null ?
-                  (<ButtonGroup>
+                {this.state.user !== null ? (
+                  <ButtonGroup>
                     <Link to="/profile">
                       <Button type="primary">
                         <Icon type="user" />
                         {"سلام " + this.state.user.Firstname}
                       </Button>
                     </Link>
-                    <Link to="/profile">
-                      <Button>
+                      <Button onClick={() => {
+                        this.toggleCreditModal(true)
+                      }}>
                         <Icon type="plus-square" />
                         اعتبار : &nbsp; {this.state.user.Credit} &nbsp; ریال
                       </Button>
-                    </Link>
                   </ButtonGroup>
-                  )
-                  :
-                  (<ButtonGroup><Link to="/login">
-                    <Button type="primary">
-                      <Icon type="unlock" />
-                      ورود
-                    </Button>
-                  </Link>
+                ) : (
+                  <ButtonGroup>
+                    <Link to="/login">
+                      <Button type="primary">
+                        <Icon type="unlock" />
+                        ورود
+                      </Button>
+                    </Link>
                     <Link to="/register">
                       <Button>
                         <Icon type="plus-square" />
                         ثبت نام
                       </Button>
                     </Link>
-                  </ButtonGroup>)
-                }
-
+                  </ButtonGroup>
+                )}
               </div>
             </div>
           </div>
         </div>
+        <Modal
+          title="افزایش اعتبار"
+          centered
+          visible={this.state.showCreditModal}
+          onOk={() => this.toggleCreditModal(false)}
+          onCancel={() => this.toggleCreditModal(false)}
+        >
+          <RadioGroup
+            onChange={this.onCreditAmountChange}
+            value={this.state.creditAmount}
+          >
+            <Radio style={radioStyle} value={5}>
+              5 هزار تومان
+            </Radio>
+            <Radio style={radioStyle} value={20}>
+              20 هزار تومان
+            </Radio>
+            <Radio style={radioStyle} value={50}>
+              50 هزارتومان
+            </Radio>
+            <Radio style={radioStyle} value={100}>
+              100 هزارتومان
+            </Radio>
+            <Radio style={radioStyle} value="other">
+              مبلغ دلخواه
+              {this.state.creditAmount === "other" ? (
+                <Input
+                  placeholder="مبلغ دلخواه را وارد کنید"
+                  onChange={e => {
+                    this.setState({ customCreditAmount: e.target.value });
+                  }}
+                  style={{ width: 220, marginRight: 20 }}
+                  addonAfter="تومان"
+                />
+              ) : null}
+            </Radio>
+          </RadioGroup>
+        </Modal>
       </div>
     );
-  } 
+  }
 }
 const styles = {
   icons: {
     fontSize: 18
   }
 };
+const radioStyle = {
+  display: 'block',
+  height: '30px',
+  lineHeight: '30px',
+};
+
 export default withRouter(HeaderDesktopComponent);
